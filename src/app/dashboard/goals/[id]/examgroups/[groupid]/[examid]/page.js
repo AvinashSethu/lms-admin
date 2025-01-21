6b8882c81a4e81c3eb5cccc83f7f4a72541f38e7
Dharaniv26 <21ec019@psr.edu.in>
2025-01-21 23:19:17 +0530

    exam routing update

--- src/app/dashboard/goals/[id]/examgroups/[groupid]/[examid]/page.js
+++ src/app/dashboard/goals/[id]/examgroups/[groupid]/[examid]/page.js
@@ -1,13 +1,11 @@
 "use client";
 import { Stack } from "@mui/material";
-import { useRouter } from "next/navigation";
 import CreateExam from "@/src/components/CreateExam/CreateExam";
 
 export default function Examid() {
-  const router = useRouter();
   return (
     <Stack  padding="20px">
-      <CreateExam title="Exam Group" />
+      <CreateExam title="Exam Group" examInfoTitle="Wednesday" />
     </Stack>
   );
 }
