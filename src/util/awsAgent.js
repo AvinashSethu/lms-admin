0759d158dc3750efdcbad4fa76c80d8484d3bf7e
AvinashSethu <61837233+AvinashSethu@users.noreply.github.com>
2025-02-19 17:52:20 +0530

    Update awsAgent.js

--- src/util/awsAgent.js
+++ src/util/awsAgent.js
@@ -3,7 +3,7 @@ import AWS from "aws-sdk";
 
 if (
   !process.env.AWS_ACCESS_KEY_ID ||
-  !process.env.AWS_SECRET_KEY ||
+  !process.env.AWS_IAM_SECRET_KEY ||
   !process.env.AWS_REGION
 ) {
   throw new Error(
@@ -15,7 +15,7 @@ if (
 AWS.config.update({
   region: process.env.AWS_REGION,
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
+  secretAccessKey: process.env.AWS_IAM_SECRET_KEY,
 });
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
